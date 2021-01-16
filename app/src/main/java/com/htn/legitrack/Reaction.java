6bfb08d973817986d5a963bcf40ee94679619c1b
jwbiernacki <jwbiernacki@gmail.com>
2021-01-16 10:40:59 -0500

    Added Firebase dependencies v5

--- app/src/main/java/com/htn/legitrack/Reaction.java
+++ app/src/main/java/com/htn/legitrack/Reaction.java
@@ -2,6 +2,7 @@ package com.htn.legitrack;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -9,9 +10,18 @@ import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 public class Reaction extends AppCompatActivity {
 
+    private static final String TAG = "Reaction";
     EditText reactionText;
+    //private static final String TAG = Reaction.class.getSimpleName();
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -24,9 +34,68 @@ public class Reaction extends AppCompatActivity {
 
     }
 
+    public void basicReadWrite() {
+        // [START write_message]
+        // Write a message to the database
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+
+        myRef.setValue("Hello, World!");
+        // [END write_message]
+
+        // [START read_message]
+        // Read from the database
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                String value = dataSnapshot.getValue(String.class);
+                Log.d(TAG, "Value is: " + value);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w(TAG, "Failed to read value.", error.toException());
+            }
+        });
+        // [END read_message]
+    }
+
     public void pushtoDB(View view) {
         // Push comment to DB
 
-    }
+            /*// [START write_message]
+            // Write a message to the database
+            FirebaseDatabase database = FirebaseDatabase.getInstance();
+            DatabaseReference myRef = database.getReference("message");
 
+            myRef.setValue("Hello, World!");
+            // [END write_message]
+
+            // [START read_message]
+            // Read from the database
+            myRef.addValueEventListener(new ValueEventListener() {
+                //private final String TAG = Reaction.class.getSimpleName();
+
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    // This method is called once with the initial value and again
+                    // whenever data at this location is updated.
+                    String value = dataSnapshot.getValue(String.class);
+                    Log.d(TAG, "Value is: " + value);
+                }
+
+                @Override
+                public void onCancelled(DatabaseError error) {
+                    // Failed to read value
+                    Log.w(TAG, "Failed to read value.", error.toException());
+                }
+            });
+            // [END read_message]
+        }*/
+
+    }
 }
+
