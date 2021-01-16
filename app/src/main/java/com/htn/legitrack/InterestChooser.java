5a30473457c2ab06c844bd0b170399a6ae803c8e
richmondvan <evan.ty.zheng@gmail.com>
2021-01-16 15:12:19 -0500

    Removed miscellaneous subjects

--- app/src/main/java/com/htn/legitrack/InterestChooser.java
+++ app/src/main/java/com/htn/legitrack/InterestChooser.java
@@ -31,9 +31,9 @@ public class InterestChooser extends AppCompatActivity {
                     "Environmental", "Executive Branch", "Family and Children Issues", "Federal, State, and Local Relations",
                     "Gambling and Gaming", "Government Reform", "Guns", "Health", "Housing and Property", "Immigration",
                     "Indigenous Peoples", "Insurance", "Judiciary", "Labor and Employment", "Legal Issues", "Legislative Affairs",
-                    "Military", "Municipal and County Issues", "Nominations", "Other", "Public Services", "Recreation",
+                    "Military", "Municipal and County Issues", "Nominations", "Public Services", "Recreation",
                     "Reproductive Issues", "Resolutions", "Science and Medical Research", "Senior Issues",
-                    "Sexual Orientation and Gender Issues", "Social Issues", "State Agencies", "Subject",
+                    "Sexual Orientation and Gender Issues", "Social Issues", "State Agencies",
                     "Technology and Communication", "Trade", "Transportation", "Welfare and Poverty"};
 
     @Override
