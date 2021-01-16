a16aafabf1e150117880a91ff9fe3834c58f3c4a
richmondvan <evan.ty.zheng@gmail.com>
2021-01-15 23:34:04 -0500

    Initial commit

---
+++ app/src/main/java/com/htn/legitrack/MainActivity.java
@@ -0,0 +1,14 @@
+package com.htn.legitrack;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
