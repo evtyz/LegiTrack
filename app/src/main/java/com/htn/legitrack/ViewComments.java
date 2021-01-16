6588c788c386e8e6fbec1ea984cc7462a743c7e0
jwbiernacki <jwbiernacki@gmail.com>
2021-01-16 17:50:05 -0500

    added a working page for viewing comments

---
+++ app/src/main/java/com/htn/legitrack/ViewComments.java
@@ -0,0 +1,62 @@
+package com.htn.legitrack;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.SeekBar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class ViewComments extends AppCompatActivity {
+
+    private static final String TAG = "ViewComments";
+
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference myRef = database.getReference("message");
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.view_comments);
+
+        //Intent intent = getIntent();
+        //newBill = (Bill) getIntent().getSerializableExtra("Bill");
+
+
+        // Read from the database
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                String value = dataSnapshot.getValue(String.class);
+                Log.d(TAG, "Value is: " + value);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w(TAG, "Failed to read value.", error.toException());
+            }
+        });
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+}
