43b52ab84b4ac34a56344f9b3014148ae5d56fdb
richmondvan <evan.ty.zheng@gmail.com>
2021-01-16 11:45:19 -0500

    Add interest chooser page

---
+++ app/src/main/java/com/htn/legitrack/InterestChooser.java
@@ -0,0 +1,18 @@
+package com.htn.legitrack;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class InterestChooser extends AppCompatActivity {
+
+    public String state;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_interest_chooser);
+        
+        // TODO: add interests
+    }
+}
