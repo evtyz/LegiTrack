7020901e8ae2168a4eafe830e6289f9f7a88aa4e
Maria Kloiber <maria.kloiber@comcast.net>
2021-01-16 12:45:38 -0600

    I think I fixed the interest selector??

--- app/src/main/java/com/htn/legitrack/InterestChooser.java
+++ app/src/main/java/com/htn/legitrack/InterestChooser.java
@@ -4,19 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
-
-import java.util.ArrayList;
 
 public class InterestChooser extends AppCompatActivity {
 
     //public static final String INTEREST_NAME = "com.htn.legitrack.INTEREST_NAME";
     public String state;
     String interestName;
-    Spinner dropdown2;
+    Spinner dialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,12 +21,12 @@ public class InterestChooser extends AppCompatActivity {
         setContentView(R.layout.activity_interest_chooser);
 
         //read in selected state information
-        Intent intent = getIntent();
-        state = intent.getStringExtra(MainActivity.STATE_NAME);
-        Log.d("test", state);
+        //Intent intent = getIntent();
+        //state = intent.getStringExtra(MainActivity.STATE_NAME);
+        //Log.d("test", state);
 
         //set spinner item id
-        dropdown2 = findViewById(R.id.spinner2);
+        dialog = findViewById(R.id.dialog);
 
         String[] interestList;
         interestList = new String[] {"Agriculture and Food", "Animal Rights and Wildlife Issues", "Arts and Humanities",
@@ -43,16 +40,16 @@ public class InterestChooser extends AppCompatActivity {
                         "Sexual Orientation and Gender Issues", "Social Issues", "State Agencies", "Subject",
                         "Technology and Communication", "Trade", "Transportation", "Welfare and Poverty"};
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, interestList);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_multiple_choice, interestList);
 
         //set the spinners adapter
-        dropdown2.setAdapter(adapter);
+        dialog.setAdapter(adapter);
 
         // TODO: add interests
     }
 
     public void sendInterest(View view) {
-        interestName = dropdown2.getSelectedItem().toString();
+        interestName = dialog.getSelectedItem().toString();
         Intent intent = new Intent(getApplicationContext(), BillDisplay.class);
         //intent.putExtra(INTEREST_NAME, interestName);
         startActivity(intent);
