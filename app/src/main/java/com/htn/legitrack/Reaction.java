b81991b4be62203b12e5ae356346eda2e8cfd50d
jwbiernacki <jwbiernacki@gmail.com>
2021-01-16 11:51:22 -0500

    Added Firebase dependencies v5

--- app/src/main/java/com/htn/legitrack/Reaction.java
+++ app/src/main/java/com/htn/legitrack/Reaction.java
@@ -77,8 +77,11 @@ public class Reaction extends AppCompatActivity {
         DatabaseReference myRef = database.getReference("message");
 
         myRef.setValue(comment);
+
         }
 
-    }
+        }
+
+
 
 
